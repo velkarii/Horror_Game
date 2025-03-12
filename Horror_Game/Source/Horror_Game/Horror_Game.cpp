75a17714c7b3444972b352007d5ff781dabb3530
Velkarii <dawbor589@gmail.com>
2025-03-12 14:34:17 +0100

    Initialization of Project

---
+++ Horror_Game/Source/Horror_Game/Horror_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Horror_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Horror_Game, "Horror_Game" );
